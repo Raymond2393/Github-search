ad7a31c0741021b4b5dd1070d38ad64d2cccb6bb
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-03-22 15:06:11 +0300

    inject http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response } from '@agular/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+constructor(private http:Http){}
+  userName="";
+  githubData:any="";
+
+  userLookup(){
+    this.http.get("https://api.github.com/users/"  + this.userName + "?access_token="+environment.access_token)
+    .subscribe(
+      (response:Response)=>{
+        const userData=response.json();
+        this.githubData=userData;
+
+
+        
+      }
+    )
+  }
+
+
   title = 'app';
 }
