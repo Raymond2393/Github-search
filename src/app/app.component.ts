8fd05625c363690e7200f5c6b113965224ed1ac8
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-19 11:39:31 +0300

    add avatar_url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {Http, Response } from '@angular/http';
-import {environment} from '../environments/environment';
+import {Http, Response} from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +9,26 @@ import {environment} from '../environments/environment';
 })
 export class AppComponent {
 
-constructor(private http:Http){}
+  constructor(private http:Http){}
   userName="";
   githubData:any="";
-
-  userLookup(){
-    this.http.get("https://api.github.com/users/"  + this.userName + "?access_token="+environment.access_token)
-    .subscribe(
-      (response:Response)=>{
-        const userData=response.json();
-        this.githubData=userData;
-        
+  imgavatarUrl="./assets/imgavatar.png";
 
 
+userLookup(){
+this.http.get("https://api.github.com/users/"+this.userName+"?access_token="+environment.access_token)
+.subscribe(
+  (response:Response)=>{
+    const userData=response.json();
+    this.githubData=userData;
 
-      }
-    )
+    this.imgavatarUrl=userData.imgavatar_url;
+    console.log(userData);
   }
+)
 
 
-  title = 'app';
+
+
+}
 }
